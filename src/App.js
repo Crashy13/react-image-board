061966d75f1eaeb485412e2e3409cae3b9b194f8
Scotty Wyatt <crashy@scottys-mbp.attlocal.net>
2021-06-02 22:45:30 -0400

    added input and buttons to form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ImageForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+    }
+    this.handleChangeText = this.handleChangeText.bind(this);
+    this.handleChangeImage = this.handleChangeImage.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChangeText(event) {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  handleChangeImage(event) {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+
+  render() {
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" name="image" value={this.state.image} onChange={this.handleChangeImage}/>
+        <input type="text" name="text" value={this.state.text} onChange={this.handleChangeText}/>
+        <button type="">Cancel</button>
+        <button type="submit">Add Image</button>
+      </form>
+    )
+  }
+};
+
+class ImageList extends Component{
+  render() {
+    return(
+      <ul>
+        <li>
+        <img src="this.state.image" alt=""/>
+        <p>Image 1</p>
+        </li>
+        <li>
+        <img src="this.state.image" alt=""/>
+        <p>Image 2</p>
+        </li>
+        <li>
+        <img src="this.state.image" alt=""/>
+        <p>Image 3</p>
+        </li>
+      </ul>
+    )
+  }
+};
+
+class ImageBoard extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: [],
+    }
+  }
+
+
+
+  render() {
+    return(
+      <div>
+        <ImageForm />
+        <ImageList />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default ImageBoard;
